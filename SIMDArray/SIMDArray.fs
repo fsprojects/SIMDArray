96d0ef8744b3482437360e706b81a64e61edf184
jackmott <jack.mott@gmail.com>
2016-07-25 12:42:37 -0500

    initial commit

---
+++ SIMDArray/SIMDArray.fs
@@ -0,0 +1,36 @@
+﻿namespace JMott.SIMDArray
+open System
+open System.Numerics
+
+
+
+module Array =
+    let inline SIMDFold<'T,'State
+                                 when 'T : (new:unit -> 'T) and 'T: struct and 'T :> ValueType
+                                 and 'State : (new:unit -> 'State) and 'State: struct and 'State :> ValueType> 
+        (f: Vector<'State> -> Vector<'T> -> Vector<'State>)  
+        (acc : 'State) 
+        (combiner : 'State -> 'State -> 'State)  
+        (combinerAcc : 'State)
+        (array:'T[]) =
+
+        match box array with 
+            | null -> nullArg "array"
+            | _ -> ()
+
+        let f = OptimizedClosures.FSharpFunc<_,_,_>.Adapt(f)
+        let mutable state = Vector<'State>(acc)
+        let mutable vi = 0
+        while vi < array.Length do
+            state <- f.Invoke(state,Vector<'T>(array,vi))
+            vi <- vi + Vector<'T>.Count
+        vi <- 0
+        let mutable result = combinerAcc
+        while vi < Vector<'State>.Count do
+            result <- combiner result state.[vi]
+            vi <- vi + 1
+        result
+
+
+
+
---
+++ TestSimd/Program.fs
@@ -0,0 +1,48 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open JMott.SIMDArray
+
+
+module test = 
+
+
+    let inline testSIMDFold f simdF c acc SIMDAcc a =
+          printf "Testing %A\n" a
+          let SIMDSum = 
+                try 
+                    Some (                                                       
+                        a 
+                        |> Array.SIMDFold simdF SIMDAcc c acc  )                    
+                with
+                    | :? System.ArgumentNullException -> None
+          let sum = 
+                try   
+                    Some (             
+                        a 
+                        |> Array.fold f acc )                                            
+                   with
+                       | :? System.ArgumentNullException -> None 
+                            
+          match (sum,SIMDSum) with
+          | (Some s, Some simd) when s <> simd -> printf "Sum Error.  SIMDSum: %A  Sum: %A\n" simd s
+          | (Some _, None) -> printf "Sum Error. simd was null sum was not\n"
+          | (None, Some _) ->  printf "Sum Error, sum was null simd was not\n"
+          | _ -> printf "Success\n"
+
+    [<EntryPoint>]
+    let main argv = 
+        let intArrays =
+          [|
+            null
+            [||]
+            [|1;2;3;4|]    
+            [|1|]
+            [|0|]
+            [|1;2;3;4;5|]
+          |]
+                
+        intArrays
+        |> Array.iter (testSIMDFold (+) (+) (+) 0 0)
+        
+        0
+            
